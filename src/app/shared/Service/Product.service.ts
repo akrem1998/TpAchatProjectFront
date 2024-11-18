62c882f3f1b6f85c1181018b08eceef05a14a32b
akrem1998 <akremboukrain19@gmail.com>
2024-11-18 12:03:14 +0100

    Update Product.service.ts

--- src/app/shared/Service/Product.service.ts
+++ src/app/shared/Service/Product.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 
 import { HttpClient} from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  readonly API_URL = '${environement.apiUrl}/product';
+  readonly API_URL = `${environement.apiUrl}/product`;
 
   constructor(private httpClient: HttpClient) { }
   getAllProducts() {

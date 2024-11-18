f4b9c6149fb18284a0ed1bd79e691a00306c5f02
akrem1998 <akremboukrain19@gmail.com>
2024-11-18 12:03:35 +0100

    Update Reglement.service.ts

--- src/app/shared/Service/Reglement.service.ts
+++ src/app/shared/Service/Reglement.service.ts
@@ -1,12 +1,13 @@
 import {Injectable} from '@angular/core';
 
 import {HttpClient} from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReglementService {
-  readonly API_URL = '${environement.apiUrl}/reglement';
+  readonly API_URL = `${environement.apiUrl}/reglement`;
 
   constructor(private httpClient: HttpClient) {
   }

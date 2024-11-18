aada834181846f71536948bec7a54ad7e06b2471
akrem1998 <akremboukrain19@gmail.com>
2024-11-18 12:01:47 +0100

    Update Facture.service.ts

--- src/app/shared/Service/Facture.service.ts
+++ src/app/shared/Service/Facture.service.ts
@@ -1,11 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FactureService {
-  readonly API_URL = '${environement.apiUrl}/facture';
+  readonly API_URL = `${environement.apiUrl}/facture`;
 
   constructor(private httpClient: HttpClient) { }
 

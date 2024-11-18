c1a5f4abb6b206be2c270b5b3effe2f583d12741
akrem1998 <akremboukrain19@gmail.com>
2024-11-18 16:00:14 +0100

    Update Product.service.ts

--- src/app/shared/Service/Product.service.ts
+++ src/app/shared/Service/Product.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 
 import { HttpClient} from '@angular/common/http';
-import { environment } from 'src/environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  readonly API_URL = `${environment.apiUrl}/product`;
+  readonly API_URL = 'http://193.95.57.13:8082/SpringMVC/produit';
 
   constructor(private httpClient: HttpClient) { }
   getAllProducts() {

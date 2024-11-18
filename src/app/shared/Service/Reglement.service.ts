b9fa8bc82a5d408ea2b0ea580fe9eb42c60f27b2
akrem1998 <akremboukrain19@gmail.com>
2024-11-18 13:47:03 +0100

    Update Reglement.service.ts

--- src/app/shared/Service/Reglement.service.ts
+++ src/app/shared/Service/Reglement.service.ts
@@ -7,7 +7,7 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root'
 })
 export class ReglementService {
-  readonly API_URL = `${environement.apiUrl}/reglement`;
+  readonly API_URL = `${environment.apiUrl}/reglement`;
 
   constructor(private httpClient: HttpClient) {
   }

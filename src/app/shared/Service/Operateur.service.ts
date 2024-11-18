9f00a4356d0840b25b193991be959799eabc32ce
akrem1998 <akremboukrain19@gmail.com>
2024-11-18 12:02:19 +0100

    Update Operateur.service.ts

--- src/app/shared/Service/Operateur.service.ts
+++ src/app/shared/Service/Operateur.service.ts
@@ -1,11 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OperateurService {
-  readonly API_URL = '${environement.apiUrl}/operateur';
+  readonly API_URL = `${environement.apiUrl}/operateur`;
 
   constructor(private httpClient: HttpClient) { }
 
